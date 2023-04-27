94ce07509f6b1c695bd8a405039e354e52531f83
Bastian Mannerow <b.mannerow@web.de>
2023-04-27 10:54:00 +0200

    Main Klasse integriert Test, ob Objekterstellung funktioniert.

--- src/main/java/tnt/Main.java
+++ src/main/java/tnt/Main.java
@@ -1,18 +1,30 @@
 package tnt;
-
 import tnt.gui.Gui;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import tnt.model.Player;
+import tnt.model.Game;
 
-/**
- * The common starting point of the GUI.
- */
 public class Main {
-    /**
-     * The external entry point of the application.
-     * @param args The command line arguments passed to the application.
-     */
     public static void main(String[] args) {
-        Gui.main(args);
+        // Kreiert testweise ein paar Playerobjekte mit zugehörigen Figuren.
+        ArrayList<Player> players = new ArrayList<Player>();
+        Game game = new Game(players);
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("Mathis", "Basti", "Nils", "Aaron"));
+        ArrayList<String> colour = new ArrayList<>(Arrays.asList("Blue", "Red", "Yellow", "Green"));
+        int playerAmount = 4;
+        int figureAmount = 5;
+        game.createPlayer(playerAmount, names, colour, figureAmount);
+
+        // Printe Ergebnisse
+        ArrayList<Player> order = game.getPlayerOrder();
+        System.out.println(order);
+        for (Player player : order) {
+            System.out.println(player.getName());
+        }
+        // Kreiert testweise ein Board mit zugehörigen Feldern.
+        int boardSize = 6;
+        game.createBoard(boardSize);
     }
 }
