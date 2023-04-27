0b25f3567f021be43e3782596e863fb5d3ec1b53
Bastian Mannerow <b.mannerow@web.de>
2023-04-27 10:46:58 +0200

    Package Pfad Korrektur. FileManager Klasse ohne Tests und Kommentare.

---
+++ src/main/java/tnt/model/FileManager.java
@@ -0,0 +1,21 @@
+package tnt.model;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class FileManager {
+    public void loadCSV() {
+        // Void muss noch zu return geändert werden
+    }
+    public void getHighscore() {
+        ArrayList<String> highscore = new ArrayList<>();
+        // zuganzahl = int;
+        // spielername = String;
+        // human = boolean;
+        // return (zuganzahl, spielername, human);
+    }
+
+    public void setHighscore(){
+        ArrayList<String> highscore = new ArrayList<>();
+    }
+
+}
