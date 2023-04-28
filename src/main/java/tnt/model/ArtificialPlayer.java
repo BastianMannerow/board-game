87ea5bfe39bf74ef01664ba9489e32234bbaf012
Bastian Mannerow <b.mannerow@web.de>
2023-04-28 11:39:40 +0200

    UML in Documentation hinzugefügt

--- src/main/java/tnt/model/ArtificialPlayer.java
+++ src/main/java/tnt/model/ArtificialPlayer.java
@@ -3,6 +3,9 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class ArtificialPlayer {
+
+    // Ändern, dass von Player geerbt wird.
+
     /**
      * Random decision making.
      *
