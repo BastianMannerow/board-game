2082e705587072cd296ed5a1c80331c0518a003e
Ludwig Rahlff <ludwig.rahlff@student.uni-luebeck.de>
2023-05-17 08:52:02 +0200

    added individual stuff to each player in playermenu

--- src/main/java/tnt/gui/playerchoosemenu/PlayerChooseView.java
+++ src/main/java/tnt/gui/playerchoosemenu/PlayerChooseView.java
@@ -2,37 +2,46 @@ package tnt.gui.playerchoosemenu;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonBase;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import tnt.ResourceHandler;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 
 public class PlayerChooseView extends VBox {
 
     private ArrayList<HBox> playerHolder = new ArrayList<HBox>();
-
+    private Color[] def_colors = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW, Color.BLACK, Color.PINK};
     public PlayerChooseView(PlayerChooseController playerChooseController) throws IOException {
         FXMLLoader choosePlayerMenu = ResourceHandler.getInstance().getFXML("choosePlayerMenu");
         choosePlayerMenu.setRoot(this);
         VBox playerChooseMenu = choosePlayerMenu.load();
-        ((VBox) this.getChildren().get(0)).setSpacing(30);
-        for(int i = 0; i< 24; i++){
+        ((ScrollPane) this.getChildren().get(0)).setFitToHeight(true);
+        ((ScrollPane) this.getChildren().get(0)).setFitToWidth(true);
+        ((VBox)((ScrollPane) this.getChildren().get(0)).getContent()).setSpacing(20);
+        ((VBox)((ScrollPane) this.getChildren().get(0)).getContent()).setPadding(new Insets(20,0,5,0));
+        for(int i = 0; i< 4; i++){
             HBox hbox1 = new HBox();
             playerHolder.add(hbox1);
-            ((VBox) this.getChildren().get(0)).getChildren().add(hbox1);
+            VBox playerBox = (VBox) ((ScrollPane) this.getChildren().get(0)).getContent();
+            playerBox.getChildren().add(hbox1);
             FXMLLoader playerLayout1 = ResourceHandler.getInstance().getFXML("choosePlayer");
             playerLayout1.setRoot(hbox1);
             playerLayout1.load();
+            ((Label) hbox1.getChildren().get(0)).setText("Player " + (i+1));
+            ((TextField)((VBox) hbox1.getChildren().get(1)).getChildren().get(1)).setPromptText("Player " + (i+1));
+            ((ColorPicker) ((VBox) hbox1.getChildren().get(3)).getChildren().get(1)).setValue(def_colors[i% def_colors.length]);
         }
 
 
