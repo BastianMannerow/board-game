ae23062d527139acd074e8d82bd8cf7049fa6d74
Peterli-Wremblelo <Peter.Wrembel@web.de>
2023-07-05 15:24:09 +0200

    added Enview and the connected fxml file

---
+++ src/main/java/tnt/gui/game/EndView.java
@@ -0,0 +1,30 @@
+package tnt.gui.game;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+import tnt.ResourceHandler;
+import tnt.gui.SceneHandler;
+
+
+import java.io.IOException;
+
+public class EndView extends VBox {
+
+    private GameController controller;
+    /**
+     * Constructor for the view
+     * @param sceneHandler the scenehandler holding all the scenes
+     * @throws IOException Exception, when fxml file could not get loaded (the file of the scenebuilder)
+     */
+    public Endview(SceneHandler sceneHandler) throws IOException {
+        FXMLLoader EndScreen = ResourceHandler.getInstance().getFXML("End");
+        EndScreen.setRoot(this);
+        EndScreen.load();
+        controller = EndScreen.getController();
+        controller.setSceneHandler(sceneHandler);
+        sceneHandler.add("End", this);
+
+    }
+
+
+}
