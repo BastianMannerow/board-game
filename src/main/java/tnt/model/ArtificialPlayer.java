27d90e7c9d8f6b12f0c4ad6e1cdc4033d77482ec
Bastian Mannerow <bastian.mannerow@student.uni-luebeck.de>
2023-07-30 02:19:49 +0200

    Punishment system integrated

--- src/main/java/tnt/model/ArtificialPlayer.java
+++ src/main/java/tnt/model/ArtificialPlayer.java
@@ -1,4 +1,5 @@
 package tnt.model;
+import java.util.List;
 import java.util.Random;
 import java.util.ArrayList;
 
@@ -55,13 +56,13 @@ public class ArtificialPlayer{
                 // For each field the following heuristic is going to be calculated, which determines the best move
                 for (Field field : possibleMoves) {
                     // If an instant win is possible, it will be executed
-                    if (moveToWin()) {
+                    if (moveToWin(field, game)) {
                         bestFigure = figure;
                         bestMove = field;
                         bestProgression = 0;
                         break;
                     }
-                    int ownProgression = game.getVictoryHeight()-field.getTowerLevel();
+                    int ownProgression = ownMoveProgressionHeuristic(figure, field, game);
                     if (ownProgression < bestProgression){
                         bestFigure = figure;
                         bestMove = field;
@@ -95,7 +96,7 @@ public class ArtificialPlayer{
                 ArrayList<Field> possibleBuilds = figure.getValidMoves(board);
                 // For each field the following heuristic is going to be calculated, which determines the best move
                 for (Field field : possibleBuilds) {
-                    int ownProgression = game.getVictoryHeight() - field.getTowerLevel();
+                    int ownProgression = ownBuildingProgressionHeuristic(figure, field, game);
                     if (ownProgression < bestProgression) {
                         bestBuild = field;
                         bestProgression = ownProgression;
@@ -143,16 +144,16 @@ public class ArtificialPlayer{
                 // For each field the following heuristic is going to be calculated, which determines the best move
                 for (Field field:possibleMoves){
                     // If an instant win is possible, it will be executed
-                    if (moveToWin()){
+                    if (moveToWin(field, game)){
                         bestFigure = figure;
                         bestMove = field;
                         bestScore = 0;
                         break;
                     }
                     // Calculating the heuristic and comparing it with the old best value
-                    int ownProgression = ownProgressionHeuristic(figure, field, game);
-                    int teamProgression = teamProgressionHeuristic(figure, field, game);
-                    int sabotageEnemy = sabotageEnemyHeuristic(figure, field, game);
+                    int ownProgression = ownMoveProgressionHeuristic(figure, field, game);
+                    int teamProgression = teamMoveProgressionHeuristic(figure, field, game);
+                    int sabotageEnemy = sabotageMoveEnemyHeuristic(figure, field, game);
 
                     // If move is better than the current best move, replace it
                     if (ownProgression + teamProgression + sabotageEnemy < bestScore){
@@ -244,20 +245,137 @@ public class ArtificialPlayer{
         }
     }
 
-    public static int teamProgressionHeuristic(Figure figure, Field field, Game game){
+    public static int teamMoveProgressionHeuristic(Figure figure, Field field, Game game){
         return 1;
     }
 
-    public static int ownProgressionHeuristic(Figure figure, Field field, Game game){
+    public static int ownMoveProgressionHeuristic(Figure figure, Field field, Game game){
         return 1;
     }
 
-    public static int sabotageEnemyHeuristic(Figure figure, Field field, Game game){
+    public static int sabotageMoveEnemyHeuristic(Figure figure, Field field, Game game){
         return 1;
     }
 
-    public static boolean moveToWin(){
+    /**
+     * Punishes if an enemy will get an advantage
+     *
+     * @param figure the figure which should be moved away
+     * @param game the game which is played on
+     *
+     * @return the punishment
+     */
+    public static int punishMovement(Figure figure, Game game){
+        int punishment = 0;
+        Field currentField = game.getBoard().getField(figure.getX(),figure.getY());
+        ArrayList<Player> allPlayer = game.getPlayerOrder();
+        for (Player player: allPlayer){
+            if(!player.getTeam().equals(figure.getOwner().getTeam())){
+                ArrayList<Figure> enemyFigures = player.getFigure();
+                for (Figure enemyFigure:enemyFigures){
+                    ArrayList<Field> possibleEnemyMoves = enemyFigure.getValidMoves(game.getBoard());
+                    if(possibleEnemyMoves.contains(currentField)){
+                        punishment = punishment+1;
+                    }
+                }
+            }
+        }
+        return punishment;
+    }
 
-        return false;
+    public static int teamBuildingProgressionHeuristic(Figure figure, Field field, Game game){
+        return 1;
+    }
+
+    public static int ownBuildingProgressionHeuristic(Figure figure, Field field, Game game){
+        return 1;
+    }
+
+    public static int sabotageBuildingEnemyHeuristic(Figure figure, Field field, Game game){
+        return 1;
+    }
+
+    /**
+     * Punishes if an enemy will get an advantage
+     *
+     * @param figure the figure which should be moved away
+     * @param field the field which will be increased
+     * @param game the game which is played on
+     *
+     * @return the punishment
+     */
+    public static int punishBuilding(Figure figure, Field field, Game game){
+        int punishment = 0;
+        ArrayList<Player> allPlayer = game.getPlayerOrder();
+        Board fictiveBord = createFictiveBoard(game.getBoard());
+        Field fictiveField = fictiveBord.getField(field.getX(), field.getY());
+        fictiveField.setTowerLevel(fictiveField.getTowerLevel()+1);
+
+        for (Player player: allPlayer){
+            if(!player.getTeam().equals(figure.getOwner().getTeam())){
+                ArrayList<Figure> enemyFigures = player.getFigure();
+                for (Figure enemyFigure:enemyFigures){
+                    ArrayList<Field> possibleEnemyMoves = enemyFigure.getValidMoves(fictiveBord);
+                    if(possibleEnemyMoves.contains(fictiveField) && fictiveField.getTowerLevel() != game.getVictoryHeight()+1){
+                        punishment = punishment+2;
+                    }
+                }
+            }
+        }
+        return punishment;
+    }
+
+    /**
+     * Checks if an instant win is possible
+     *
+     * @param field the field in reach
+     * @param game the game which is played on
+     *
+     * @return the boolean
+     */
+    public static boolean moveToWin(Field field, Game game){
+        if (field.getTowerLevel() == game.getVictoryHeight()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     * Creates a fictive board to perform operations on. It is a copy of the given board
+     *
+     * @param originalBoard the game which data needs to be duplicated
+     *
+     * @return the fictive board
+     */
+    public static Board createFictiveBoard(Board originalBoard){
+        // Duplicating the fields first
+        ArrayList<Field> allFields = new ArrayList<>();
+        int xSize = originalBoard.getXSize();
+        int ySize = originalBoard.getYSize();
+        for (int i = 0; i < xSize; i++){
+            for (int j = 0; j < ySize; j++) {
+                Field fieldObject = new Field(i, j);
+                fieldObject.setTowerLevel(originalBoard.getField(i,j).getTowerLevel());
+                fieldObject.setTowerComplete(Boolean.valueOf(originalBoard.getField(i,j).getTowerComplete()));
+                allFields.add(fieldObject);
+            }
+        }
+
+        // Finalising the board object
+        Field[][] fields = new Field[xSize][ySize];
+        for (int i = 0; i < xSize; i++) {
+            for (int j = 0; j < ySize; j++) {
+                Field field = new Field(i, j);
+                fields[i][j] = field;
+            }
+        }
+        Board fictiveBoard = new Board(fields, xSize, ySize);
+        fictiveBoard.setRoundWorld(originalBoard.getRoundWorld());
+        for (Field field: allFields){
+            fictiveBoard.setField(field.getX(), field.getY(), field);
+        }
+        return fictiveBoard;
     }
 }
